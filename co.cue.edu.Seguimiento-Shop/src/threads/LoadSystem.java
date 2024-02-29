9d2ec6fa60156cb319a3e146cc16906309b2fd73
Alzate205 <132091640+Alzate205@users.noreply.github.com>
2024-02-29 00:13:41 -0500

    Add files via upload

---
+++ co.cue.edu.Seguimiento-Shop/src/threads/LoadSystem.java
@@ -0,0 +1,25 @@
+package threads;
+
+import services.ToysServiceImpl;
+
+import java.util.concurrent.Callable;
+
+public class LoadSystem implements Callable<Void> {
+
+    public LoadSystem (ToysServiceImpl toyStore) {
+    }
+
+    @Override
+    public Void call() throws Exception {
+        try {
+            System.out.println("Starting system ...");
+            Thread.sleep(1000);
+            System.out.println("system loaded successfully.");
+        } catch (InterruptedException e) {
+            System.err.println("System interrupted.");
+        } catch (Exception e) {
+            System.err.println("Error while loading the system: " + e.getMessage());
+        }
+        return null;
+    }
+}
